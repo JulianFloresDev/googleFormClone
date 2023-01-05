ec1632b9e027bffd19997dfcfb87f070c7ad790d
Julian Flores <julianfloresdev@gmail.com>
2023-01-05 14:03:43 -0300

    B-02: create Router structure and components for render

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,8 +1,20 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { Form, Awnser } from "Components";
+
 const Layout = () => {
   return (
-    <div>
-      <input type='text' value='first render after clear work space' />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/register" component={Form} />
+        <Route exact path="/user-awnser" component={Awnser} />
+        <Redirect to="/register" />
+      </Switch>
+    </Router>
   );
 };
 
---
+++ src/Components/index.js
@@ -0,0 +1,5 @@
+import Form from "./Form";
+import Layout from "./Layout";
+import Awnser from "./Awnser";
+
+export { Form, Layout, Awnser };
